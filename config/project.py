e94e3c688a7335ce10a67ba60fd6ccdb509f1e02
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:34:03 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -22,7 +22,7 @@ project_keywords = [
     "scons",
 ]
 project_license = "MIT"
-project_year_started = "2020"
+project_year_started = 2020
 project_description = project_short_description
 project_platforms = [
     "python3",
