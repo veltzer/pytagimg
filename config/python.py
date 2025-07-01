f4da0602c06e7034f9a9446585efc32928800e64
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:17 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,26 +1,15 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pytagimg": "pytagimg.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pytagimg": "pytagimg.main:main",
+}
